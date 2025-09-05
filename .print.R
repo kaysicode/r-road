55e62f0e67b6c29bf49b60abb9d4f03b12dcf860
Kenneth Mayo <2242754@slu.edu.ph>
2025-09-06 06:41:30 +0800

    feat: added print.R

---
+++ .print.R
@@ -0,0 +1,22 @@
+# This R Program will show on how to print and
+# what is the syntax in R
+
+
+# Both are print statement, first you can simply just print without using the
+# print(). But in some cases, you need to use the print() when working with
+# for loops
+
+"Hi R World!"
+print("Hello R World!")
+
+# Notice that, when you clicked the Run, it will only show the "Hi R World!"
+# This is because, the Run will always shows the line by line of the program
+# even the comments.
+# before it moves to the next one. However, if you highlight a code then,
+# that code will shows first. 
+
+# If you want to run all of the program, click the Source. But it will only shows the
+# output that has print() or cat()
+
+# to clear the console type ctrl + l
+
