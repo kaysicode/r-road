e2cf3828e2113a5df0e9b8ad13ae049dcfc1762e
Kaysi Code <codeceorl@gmail.com>
2025-09-08 12:55:04 +0800

    feat: added relationalOperators.R and finished assignVariables.R

--- topics/.assignVariables.R
+++ topics/.assignVariables.R
@@ -3,4 +3,29 @@
 
 x <- 10
 y = 20
-assign("z", 30) # First put what variable then the value 
+assign("z", 30) # First put what variable then the value
+
+# In R, there's a Predefined Constant that already have value
+# These are;
+# pi, letters, LETTERS, month.name, month.abb
+
+num <- pi
+print(num)
+class(num)
+
+small_letters <- letters
+print(small_letters)
+class(small_letters)
+
+big_letters <- LETTERS
+print(big_letters)
+class(big_letters)
+
+name_of_months <- month.name
+print(name_of_months)
+class(name_of_months)
+
+abbreviate_of_months <- month.abb
+print(abbreviate_of_months)
+class(abbreviate_of_months)
+mode(abbreviate_of_months)
---
+++ topics/.relationalOperators.R
@@ -0,0 +1,25 @@
+# Operators:
+# < : Less Than
+# <= : Less Than Equal To
+# > : Greater Than
+# >= : Greater Than Equal To
+# == : Equal To
+# != Not Equal To
+
+# The result will be in logical values (TRUE/FALSE)
+
+x <- 20
+y <- 10
+i <- 10
+
+# The value of Z is logical value
+z <- x < 10
+print(z)
+z <- y <= i
+print(z)
+z <- y >= i
+print(z)
+z <- x > i
+print(z)
+z <- y != i
+print(z)
