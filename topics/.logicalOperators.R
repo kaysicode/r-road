042d7483109b25075093acae648ccf6528ff3a38
Kaysi Code <codeceorl@gmail.com>
2025-09-09 15:46:13 +0800

    feat: added .logicalOperators.R

---
+++ topics/.logicalOperators.R
@@ -0,0 +1,13 @@
+# Operators:
+# & : AND
+# && : AND (cannot use for vectorized operations)
+# | : OR
+# || : OR (cannot use for vectorized operations)
+# ! : NOT
+
+compare1 <- TRUE
+compare2 <- TRUE
+result <- compare1 != compare2
+print(result)
+
+print(!FALSE)
