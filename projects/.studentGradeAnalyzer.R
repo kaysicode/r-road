000010998605843ef7f1ca4244c65383480dbf81
Kaysi Code <codeceorl@gmail.com>
2025-09-09 13:27:31 +0800

    docs: added a pseudo code / intructions at .studentGradeAnalyzer.R

--- projects/.studentGradeAnalyzer.R
+++ projects/.studentGradeAnalyzer.R
@@ -1,1 +1,16 @@
 # Initial Output muna HAHAHAHHA
+# So this is the plan
+# first, we have 2 function/method 
+# one for console, and how it will compute the grade
+# and one for the plot or graphical interpretation
+
+# console - Ian
+# Console:
+# > The program will ask for how many number of student would be there
+#   - exception: only integer / numeric can be accepted
+# > The program will ask for the prelims, midterms, and tentative finals
+#   grade for each student.
+#   - exception: only integer / numeric can be accepted
+# > The program will compute the final grade
+
+# UI - Me
